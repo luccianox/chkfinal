64c6fff8d27277de94b6bf780330376458e0373a
luccianox <kevinnambubbi@gmail.com>
2025-09-28 18:53:20 +0300

    countalpha

--- countalpha.go
+++ countalpha.go
@@ -8,8 +8,6 @@ Expected functions
 */
 package main
 
-import "fmt"
-
 func CountAlpha(s string) int {
 
 	count := 0
@@ -22,9 +20,4 @@ func CountAlpha(s string) int {
 		}
 	}
 	return count
-}
-func main() {
-	fmt.Println(CountAlpha("Hello world"))
-	fmt.Println(CountAlpha("H e l l o"))
-	fmt.Println(CountAlpha("H1e2l3l4o"))
 }
