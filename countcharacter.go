7b27f5bdd9c8cf0a91f8af08ab55f66f3678c0df
luccianox <kevinnambubbi@gmail.com>
2025-09-28 23:25:20 +0300

    countcharacter

--- countcharacter.go
+++ countcharacter.go
@@ -11,8 +11,6 @@ Expected Function
 */
 package main
 
-import "fmt"
-
 func CountChar(str string, c rune) int {
 	count := 0
 
@@ -23,10 +21,4 @@ func CountChar(str string, c rune) int {
 		}
 	}
 	return count
-}
-func main() {
-	fmt.Println(CountChar("Hello World", 'l'))
-	fmt.Println(CountChar("5  balloons", 5))
-	fmt.Println(CountChar("   ", ' '))
-	fmt.Println(CountChar("The 7 deadly sins", '7'))
 }
