e18f51a94e7da0b7844ab69ad01c677db41f9f54
luccianox <kevinnambubbi@gmail.com>
2025-09-28 20:57:33 +0300

    chunk

--- chunk.go
+++ chunk.go
@@ -32,10 +32,3 @@ func Chunk(slice []int, size int) {
 	}
 	fmt.Println(result)
 }
-func main() {
-	Chunk([]int{}, 10)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
-}
