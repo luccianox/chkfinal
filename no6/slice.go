abcdec11e088f3f2b8dfc1ebb3515909d2c13988
luccianox <kevinnambubbi@gmail.com>
2025-10-02 04:02:52 +0300

    slice

---
+++ no6/slice.go
@@ -0,0 +1,53 @@
+/*
+slice
+Instructions
+The function receives a slice of strings and one or more integers, and returns a slice of strings.
+
+	The returned slice is part of the received one but cut from the position indicated in the first int,
+	 until the position indicated by the second int.
+
+In case there only exists one int, the resulting slice begins in the position indicated by
+the int and ends at the end of the received slice.
+
+The integers can be negative.
+
+Expected function
+*/
+package main
+
+func Slice(a []string, nbrs ...int) []string {
+
+	n := len(a)
+	if len(nbrs) < 0 || len(nbrs) > 2 {
+		return nil
+	}
+	// one indice
+
+	if len(nbrs) == 1 {
+
+		start := nbrs[0]
+		if start < 0 {
+
+			start = n + start
+		}
+		if start < 0 || start >= n {
+			return nil
+		}
+		return a[start:]
+	}
+	// 2 indices
+
+	start, end := nbrs[0], nbrs[1]
+	if start < 0 {
+		start = start + n
+	}
+	if end < 0 {
+
+		end = end + n
+	}
+	if start < 0 || start >= n || start >= end || end < 0 || end > n {
+
+		return nil
+	}
+	return a[start:end]
+}
